280a1417ccc7eaa904a58a631f02a54d5ffbfecc
MaksRam <104136006+MaksRam@users.noreply.github.com>
2022-09-22 01:08:22 +0300

    sl

---
+++ hei.js
@@ -0,0 +1,9 @@
+const toBeTagged = 'U0408RXL459'
+
+const arrayWithMarks = [
+    'white_check_mark',
+    'heavy_check_mark',
+    'rainbow_indicator_check_mark',
+    'checkmark_green'
+]
+module.exports = {toBeTagged, arrayWithMarks}
